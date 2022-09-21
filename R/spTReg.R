fab188c8c0f976f1d32a3ef7cee4d38112fe3495
Jorge Castillo-Mateo <jorgecastillomateo@gmail.com>
2022-09-21 19:52:15 +0200

    First Step ...

---
+++ R/spTReg.R
@@ -0,0 +1,38 @@
+#' @title \strong{spTReg}: Spatio-Temporal Mean and Quantile autoRegression
+#' @aliases spTReg-package
+#' @aliases spTReg
+#' @description 
+#'   \strong{spTReg} uses different hierarchical Bayesian 
+#'   spatio-temporal modeling strategies with spatially varying coefficients, 
+#'   namely:
+#'   
+#'   (1) Mean regression using Gaussian processes and Gaussian errors,
+#'   
+#'   (2) Quantile regression using Gaussian processes and asymmetric Laplace errors.
+#'   
+#' @details 
+#'   The back-end code of this package is built under \code{C++} language.
+#'   
+#'   Main functions used: 
+#'   
+#'   (1) \code{\link{spTMean}} and \code{\link{predict.spTMean}}
+#'   
+#'   (2) \code{\link{spTQuantile}} and \code{\link{predict.spTQuantile}}
+#'   
+#' @docType package
+#' @author Jorge Castillo-Mateo <jorgecastillomateo@gmail.com>
+#' @references 
+#' Castillo-Mateo J, Lafuente M, Asín J, Cebrián AC, Gelfand AE, Abaurrea J (2022). 
+#' Spatial modeling of day-within-year temperature time series: an examination of daily maximum temperatures in Aragón, Spain. 
+#' \emph{Journal of Agricultural, Biological and Environmental Statistics}, \strong{27}(3), 487--505. 
+#' \doi{10.1007/s13253-022-00493-3}
+#' 
+#' Castillo-Mateo J, Asín J, Cebrián AC, Gelfand AE, Abaurrea J. 
+#' Spatial quantile autoregression for season within year daily maximum temperature data. 
+#' \emph{(Under review.)}
+#' 
+#' @import Rcpp
+#' @importFrom Rcpp evalCpp
+#' @useDynLib spTReg
+#' @name spTReg-package
+NULL
---
+++ src/code_fit_models.cpp
@@ -0,0 +1,15 @@
+#include "RcppArmadillo.h"
+
+// [[Rcpp::depends(RcppArmadillo)]]
+
+// [[Rcpp::export]]
+double spTMeanRcpp() {
+
+    return 1;
+}
+
+// [[Rcpp::export]]
+double spTQuantileRcpp() {
+  
+  return 1;
+}
